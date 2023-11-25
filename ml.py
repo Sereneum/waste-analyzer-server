b180c8de854f8f9aea6dac13823017867e9c2462
Sereneum <54134545+Sereneum@users.noreply.github.com>
2023-11-25 11:46:26 +0300

    limits

--- ml.py
+++ ml.py
@@ -20,7 +20,7 @@ class YoloModel:
         for file in files:
             names = {0: '-', 1: 'Beton', 2: 'Derevo', 3: 'Grunt', 4: 'Kirpich'}
             counter = {0: CntItem(), 1: CntItem(), 2: CntItem(), 3: CntItem(), 4: CntItem()}
-            trimmed_clip = super_video_format(file[1], file[0], 120, 140)
+            trimmed_clip = super_video_format(file[1], file[0], 125, 145)
             for frame in trimmed_clip.iter_frames(10):
                 results = self.model(frame)
                 for r in results:
