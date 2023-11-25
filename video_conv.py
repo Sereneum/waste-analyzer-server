eaa10c7931ef75248fa0d2d5a97f1d7403d28325
Sereneum <54134545+Sereneum@users.noreply.github.com>
2023-11-25 13:15:33 +0300

    Update video_conv.py

--- video_conv.py
+++ video_conv.py
@@ -5,15 +5,14 @@ import os
 def super_video_format(file_data, index="", start=0, end=0):
     file_path = temp(file_data, index)
     video_clip = VideoFileClip(file_path)
-    try:
-        duration = video_clip.duration
-        if duration < end:
-            return video_clip.subclip(start, end)
-        else:
-            return video_clip
-    except:
-        return video_clip
-
+    duration = video_clip.duration
+    if duration < 140:
+        s = duration / 2 - 10
+        e = s + 20
+    else:
+        s = start
+        e = end
+    return video_clip.subclip(s, e)
 
 
 def temp(file_data, index=""):
